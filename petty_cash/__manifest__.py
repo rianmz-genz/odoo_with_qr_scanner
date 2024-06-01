3626c46f941eca919ac19fc971014b1885915e82
5peedDem0n <felistigris13@gmail.com>
2024-06-01 16:47:53 +0700

    update

--- petty_cash/__manifest__.py
+++ petty_cash/__manifest__.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "PettyCash",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'name': "Petty Cash",
+    'summary': """Sistem Kas Kecil""",
 
     'description': """
-        Long description of module's purpose
+        Modul ini digunakan untuk menjalankan sistem pencatatan kas kecil
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'sequence': -100,
+    'author': "Felis, Tugas Akhir",
+    # 'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting/Accounting',
     'version': '0.1',
+    'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web', 'sale', 'board', 'account', 'l10n_id'],
@@ -48,4 +46,8 @@
             'petty_cash/static/src/components/**/*.scss',
         ],
     },
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
