6ddc4748e540c622c8954a9fb5b9b7bfa058d568
5peedDem0n <felistigris13@gmail.com>
2024-06-01 18:36:23 +0700

    update

--- petty_cash/__manifest__.py
+++ petty_cash/__manifest__.py
@@ -13,7 +13,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Accounting/Accounting',
+    'category': 'Accounting',
     'version': '0.1',
     'license': 'LGPL-3',
 
